4cedc63817e27606674f47cc1e779151687d3ac7
mpifa <pifarremarc@gmail.com>
2014-03-20 16:35:25 +0100

    Testing sensors

--- src/main/ResultActivity.java
+++ src/main/ResultActivity.java
@@ -7,33 +7,40 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
 
 public class ResultActivity extends Activity implements SensorEventListener {
 
-	SensorManager sManager;
-	Sensor sensor;
-
+	private SensorManager sManager;
+	private Sensor sensor;
+	private Item item;
+	private TextView result;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result);
 
-		Item item = getIntent().getParcelableExtra("Sensor");
-
+		this.item = getIntent().getParcelableExtra("Sensor");
+		this.result = (TextView) findViewById(R.id.ResultView);
 		this.sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		this.sensor = sManager.getDefaultSensor(item.getSensorType());
 
 		if (sensor != null && item.getName().equals("Altitud")) {
 			//Sensor 6
+			result.setText(sensor.toString());
 		} else if (sensor != null && item.getName().equals("Barometro")) {
 			//Sensor 6
+			result.setText(sensor.toString());
 		} else if (sensor != null && item.getName().equals("Humedad")) {
-			System.out.println(sensor.getType());
+			//Sensor 12
+			result.setText(sensor.toString());
 		} else if (sensor != null && item.getName().equals("Temperatura")) {
-			System.out.println(sensor.getType());
+			//Sensor 13
+			result.setText(sensor.toString());
 		} else if (item.getSensorType() == 999) {
+			result.setText("SONIDO");
 		}
 
 	}
@@ -45,17 +52,20 @@ public class ResultActivity extends Activity implements SensorEventListener {
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		/*if (event.sensor.getType()) {
-
-		} else if (event.sensor.getType()) {
-
-		} else if (event.sensor.getType()) {
-			
-		} else if (event.sensor.getType()) {
-
+		float[] values = event.values;
+		
+		if (event.sensor.getType() == 6) {
+			result.setText(Float.toString(values[0])+
+					"\n"+Float.toString(values[1]));
+		} else if (event.sensor.getType() == 6) {
+			result.setText(sensor.toString());
+		} else if (event.sensor.getType() == 12) {
+			result.setText(sensor.toString());
+		} else if (event.sensor.getType() == 13) {
+			result.setText(sensor.toString());
 		} else if (item.getSensorType() == 999) {
 
-		}*/
+		}
 	}
 
 	@Override
